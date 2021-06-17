1257688e2e815546282984c538ac42361a639b93
Omar Abdulkhalek <polatak312@gmail.com>
2021-06-17 13:03:52 +0200

    corrected

--- db/db.sql
+++ db/db.sql
@@ -1,27 +1,29 @@
-CREATE DATABASE messstation;
+CREATE DATABASE IF NOT EXISTS messstation DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-CREATE TABLE Gebaeude{
+
+
+USE DATABASE messstation;
+CREATE TABLE Gebaeude(
     Ge_Nr INT PRIMARY KEY AUTO_INCREMENT,
     Gebaeude_Name varchar(100) NOT NULL UNIQUE
-}
+)
 
-
-CREATE TABLE Raum{
+CREATE TABLE Raum(
     Ra_Nr INT PRIMARY KEY AUTO_INCREMENT,
     Raum_Name varchar(100) NOT NULL UNIQUE
-}
+)
 
-CREATE TABLE Position{
+CREATE TABLE Position(
     Pos_Nr INT PRIMARY KEY AUTO_INCREMENT,
     Position varchar(150) NOT NULL UNIQUE
-}
+)
 
-CREATE TABLE Arten_Messungen{
+CREATE TABLE Arten_Messungen(
     Art_ID INT PRIMARY KEY AUTO_INCREMENT,
     Art_Name varchar(150) NOT NULL UNIQUE
-}
+)
 
-CREATE TABLE Messung{
+CREATE TABLE Messung(
     Messung_ID INT PRIMARY KEY AUTO_INCREMENT,
     Zeitpunkt DATETIME,
     Messung_WERT varchar(100) NOT NULL UNIQUE,
@@ -29,8 +31,8 @@ CREATE TABLE Messung{
     Ra_Nr INT,
     Pos_Nr INT,
     Art_ID INT,
-    FOREIGN KEY (Ge_Nr) REFERENCES Gebaeude(Ge_Nr),
-    FOREIGN KEY (Ra_Nr) REFERENCES Raum(Ra_Nr),
-    FOREIGN KEY (Pos_Nr) REFERENCES Position(Pos_Nr),
-    FOREIGN KEY (Art_ID) REFERENCES Arten_Messungen(Art_ID)
-}
+    FOREIGN KEY (Ge_Nr) REFERENCES Gebaeude(Ge_Nr) ON DELETE ON UPDATE,
+    FOREIGN KEY (Ra_Nr) REFERENCES Raum(Ra_Nr) ON DELETE ON UPDATE,
+    FOREIGN KEY (Pos_Nr) REFERENCES Position(Pos_Nr) ON DELETE ON UPDATE,
+    FOREIGN KEY (Art_ID) REFERENCES Arten_Messungen(Art_ID) ON DELETE ON UPDATE
+)
