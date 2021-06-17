6c8ca7f0b621c972d4afd79e3d75a5b5a805df98
Omar Abdulkhalek <polatak312@gmail.com>
2021-06-17 10:33:04 +0200

    Verbindung mit der Datenbank herstellen. Die Zugriff erfolgt dann über die Variable $con.

---
+++ db/Connection.php
@@ -0,0 +1,63 @@
+<?php
+
+class connection{
+    private $servername;
+    private $dbname; 
+    private $dbusername;
+    private $dbpassword;
+    private $dns; // //Data Source Name
+    private $con;
+
+
+    function __construct()
+    {
+        $this->servername = "";
+        $this->dbname = "";
+        $this->dbusername = "";
+        $this->dsn = "mysql:host=$this->servername;dbname=$this->dbname";
+        $this->setConnection();
+    }
+
+    function setConnection()
+    {
+        $con = null;
+        try{
+            $con = new PDO($this->dns, $this->dbusername, $this->dbpassword);
+            $this->__set("con", $con);
+        }catch(PDOException $e){
+            echo "Connection Failed: " . $e->getMessage();
+            $con == null;
+        }
+    }
+
+    function __get($name)
+    {
+        switch ($name) {
+            case 'con':
+                return $this->con;
+            default:
+                    echo "<br/> Failed occurred switch __get($name) <br/>";
+                break;
+        }
+    }
+
+    function __set($name, $value)
+    {
+        switch ($name) {
+            case 'con':
+                $this->con = $value;
+                break;
+        }
+        return $value;
+    }
+
+
+    //die Connection to Database after skript executed
+    function __destruct()
+    {
+        $this->__set("con", null);
+    }
+
+} 
+
+?>
