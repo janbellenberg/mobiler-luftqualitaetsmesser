82bb6babba1ce94b8116139cd498199b87caa040
Jan Bellenberg <janbellenberg.jb@gmail.com>
2021-06-17 13:09:49 +0200

    implemented new api

--- react/mobiler-luftqualitaetsmesser/src/App.js
+++ react/mobiler-luftqualitaetsmesser/src/App.js
@@ -5,30 +5,44 @@ import './index.css';
 const App = () => {
   
   const [date, setDate] = useState(new Date().toISOString().slice(0, 10));
+  const [tLabels, setTLabels] = useState([]);
   const [temperature, setTemperature] = useState([]);
   const [humidity, setHumidity] = useState([]);
+  const [hLabels, setHLabels] = useState([]);
   const [co2, setCo2] = useState([]);
-  const [labels, setLabels] = useState([]);
+  const [cLabels, setCLabels] = useState([]);
 
   const updateData = useCallback(async () => {
     let tmp = await fetch("http://localhost/data.json?date=" + date );
     tmp = await tmp.json();
     let t = [];
+    let tL = [];
     let h = [];
+    let hL = [];
     let c = [];
-    let l = [];
+    let cL = [];
 
-    tmp.forEach((item) => {
-      t.push(item.temperature);
-      h.push(item.humidity);
-      c.push(item.co2);
-      l.push(item.timestamp);
+    tmp.temperature.forEach((item) => {
+      t.push(item.value);
+      tL.push(item.timestamp);
+    });
+
+    tmp.humidity.forEach((item) => {
+      h.push(item.value);
+      hL.push(item.timestamp);
+    });
+
+    tmp.co2.forEach((item) => {
+      c.push(item.value);
+      cL.push(item.timestamp);
     });
 
     setTemperature(t);
+    setTLabels(tL);
     setHumidity(h);
+    setHLabels(hL);
     setCo2(c);
-    setLabels(l);
+    setCLabels(cL);
   }, [date]);
 
   useEffect(() => {
@@ -49,19 +63,19 @@ const App = () => {
       <LineChart 
         title="Temperatur"
         data={temperature}
-        labels={labels}
+        labels={tLabels}
         color="#ff8c00" />
 
       <LineChart 
         title="Luftfeuchte"
         data={humidity}
-        labels={labels}
+        labels={hLabels}
         color="#156ea6" />
 
       <LineChart 
         title="CO2"
         data={co2}
-        labels={labels}
+        labels={cLabels}
         color="#e800e8" />
     </div>
   );
