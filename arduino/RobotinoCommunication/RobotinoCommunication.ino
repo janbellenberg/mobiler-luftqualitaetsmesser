8004f26415c4eac712497d02a55c22d52d6ebbb9
Jan Bellenberg <janbellenberg.jb@gmail.com>
2021-06-23 12:28:12 +0200

    Arduino Communication with interrupt

--- arduino/RobotinoCommunication/RobotinoCommunication.ino
+++ arduino/RobotinoCommunication/RobotinoCommunication.ino
@@ -1,16 +1,34 @@
+#include <avr/interrupt.h>
+
 void setup() {
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
+  pinMode(7, INPUT_PULLUP);
   Serial.begin(9600);
+
+  cli();
+  PCICR |= 0b00000100;
+  PCMSK2 |= 0b10000000;
+  sei();
 }
 
 void loop() {
-  byte value = 0;
+}
 
-  for(byte i = 0; i < 5; i++) {
-    byte tmp = digitalRead(i + 2);
-    if(tmp == HIGH) {
-      value |= (0x01 << i);
+ISR(PCINT2_vect) {
+  if(digitalRead(7) == HIGH){
+    byte value = 0;
+  
+    for(byte i = 0; i < 5; i++) {
+      byte tmp = digitalRead(i + 2);
+      if(tmp == HIGH) {
+        value |= (0x01 << i);
+      }
     }
+  
+    Serial.println(value);
   }
-
-  Serial.println(value);
 }
