dfb9dbcfab08d478fced0bcffaedb31766c6495c
Omar Abdulkhalek <polatak312@gmail.com>
2021-06-24 14:07:16 +0200

    getestet alles in Ordnung

--- db/get_Data.php
+++ db/get_Data.php
@@ -16,39 +16,27 @@
         if ($con != null) {
             try {
                 $sql = "SELECT co2_Wert, temperatur_Wert, feuchtigkeit_Wert,
-                        Pos_Nr, DATE_FORMAT(cTimestamp, '%H.%i') AS 'time' FROM Messung 
+                        Pos_Nr, DATE_FORMAT(cTimestamp, '%H:%i') AS 'time' FROM Messung 
                         WHERE
                         DATE_FORMAT(cTimestamp,'%Y-%m-%d') = '$data' 
                         AND
                         Pos_Nr = $position ;"; 
     
                 $result = $con->query($sql)->fetchAll();
+                $resultArray = array();
 
                 if ($result != null) {
                         foreach ($result as $row) {
-                            
-                            
                             $response = array(
                                 'temperature' => (float) $row["temperatur_Wert"],
                                 'humidity' => (float) $row["feuchtigkeit_Wert"],
                                 'co2' => (float) $row["co2_Wert"],
-                                'timestamp' =>  (float) $row["time"]
+                                'timestamp' =>  $row["time"]
                             );
-
-                            //$String_response = implode(",", $response);
-                            //echo "<br> $String_response <br>";
-
-                            if ($response != null) {
-                                $json =  json_encode($response);
-                                if ($json === false) {
-                                    http_response_code(500);
-                                }else echo $json;
-                            }              
-                          
+                            array_push($resultArray, $response);                          
                         } 
-                }
-
-    
+                    }
+                echo json_encode($resultArray);
             } catch (PDOException $e) {
                 echo "Connection Failed: " . $e->getMessage();
                 $con = null;
@@ -59,46 +47,38 @@
     if (isset($_GET["locations"])) {
         //$building = $_GET["building"];
         //Connection DB
+        $db = new Connection();
         $con = $db->__get("con");
 
         $response = null;
 
         if ($con != null) {
             try {
-                $sql = "SELECT Gebaeude_Name FROM gebaeude;";
+                $sql = "SELECT Ge_Nr,Gebaeude_Name FROM gebaeude;";
 
                 $result = $con->query($sql)->fetchAll();
+                $resultArray = array();
 
                 if ($result != null) {
                     foreach ($result as $row) {
-                        $Gebaeude_Name = $row["Gebaeude_Name"];
 
                         $response = array(
-                            "id" => $Ge_Nr,
-                            "building" => $Gebaeude_Name
+                            "id" => (int)$row["Ge_Nr"],
+                            "name" => $row["Gebaeude_Name"]
                         );
-
-                        if ($response != null) {
-                            $json = json_encode($response);
-                            if ($json === false) {
-                                http_response_code(500);
-                            }else echo $json;
-                        }
+                        array_push($resultArray, $response);
                     }
                 }
-
+                echo json_encode($resultArray);
             } catch (PDOException $e) {
                 echo "Connection Failed: " . $e->getMessage();
                 $con = null;
             }
         }
-
-
-
     }
 
-    if (isset($_GET["rooms"])) {
-        //$room = $_GET["room"];
+    if (isset($_GET["location_id"])) {
+        $location_id = $_GET["location_id"];
         //Connection DB
         $con = $db->__get("con");
 
@@ -106,28 +86,18 @@
 
         if ($con != null) {
             try {
-                $sql = "SELECT Raum_Name FROM raum;";
+                $sql = "SELECT Raum_Name FROM raum WHERE Ge_Nr = $location_id;";
                 
                 $result = $con->query($sql)->fetchAll();
+                $resultArray = array();
                 
                 if ($result != null) {
                     foreach ($result as $row) {
-                        $room = $row["Raum_Name"];
-
-                        $response = array(
-                            $room
-                        );
-
-                        if ($response != null) {
-                            $json = json_encode($response);
-                            if ($json === false) {
-                                http_response_code(500);
-                            }else echo $json;
-                        }
+                        array_push($resultArray, (int)$row["Raum_Name"]); 
                     }
+                    
                 }
-
-                
+                echo json_encode($resultArray);
             } catch (PDOException $e) {
                 echo "Connection Failed: " . $e->getMessage();
                 $con = null;
@@ -135,8 +105,8 @@
         }
     }
 
-    if (isset($_GET["positions"])) {
-        //$position_Nr = $_GET["position"];
+    if (isset($_GET["room_id"])) {
+        $room_id = $_GET["room_id"];
         //Connection DB
         $con = $db->__get("con");    
 
@@ -144,30 +114,31 @@
 
         if ($con != null) {
             try {
-                $sql = "SELECT Position FROM positionstelle";            
+                $sql = "SELECT Pos_Nr FROM positionstelle INNER JOIN raum ON raum.Ra_Nr = positionstelle.Ra_Nr WHERE raum.Raum_Name = $room_id";
                 $result = $con->query($sql)->fetchAll();
+                $resultArray = array();
                     
                 if ($result != null) {
                     foreach ($result as $row) {
-                        $position = $row["Position"];
-    
-                        $response = array(
-                            $position
-                        );
-    
-                        if ($response != null) {
-                            $json = json_encode($response);
-                            if ($json === false) {
-                                http_response_code(500);
-                            }else echo $json;
-                        }
+                        array_push($resultArray, (int)$row["Pos_Nr"]);
                     }
+                    
                 }
-
+                echo json_encode($resultArray);
             } catch (PDOException $e) {
                 echo "Connection Failed: " . $e->getMessage();
                 $con = null;
             }
         }
     }
+
+    /*
+    if ($response != null) {
+                            $json = json_encode($response);
+                            if ($json === false) {
+                                http_response_code(500);
+                            }else echo $json;
+    }
+    */
+
 ?>
